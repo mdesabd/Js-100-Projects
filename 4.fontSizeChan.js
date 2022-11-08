740d8200b4cccfbc95b4c58e95d75e8d074e9798
mdesabd <esa.ce2020@gmail.com>
2022-11-08 11:12:55 +0600

    text

---
+++ 4.fontSizeChan.js
@@ -0,0 +1,4 @@
+document.getElementById('4.FontSizeChange').addEventListener('click', function () {
+    const fontlarge = document.getElementById('textAlignCenter')
+    fontlarge.style.fontSize = "60px";
+})
